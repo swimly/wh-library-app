6cbe931fad2788130571d334d6324594dfefd50f
swimly <979741120@qq.com>
2016-06-01 20:42:12 +0800

    滚动tab
    滚动tab

--- js/main.js
+++ js/main.js
@@ -4,14 +4,26 @@
 init();
 function init(){
     var hd=$(".tabBox .hd").find("ul");
-    console.log(hd.children().length)
     var wid=0;
+    var wArr=[];
     for(var i=0;i<hd.children().length;i++){
         wid+=hd.children().eq(i).width();
     }
-    console.log(wid)
     hd.width(wid);
+    TouchSlide({
+        slideCell:"#leftTabBox",
+        startFun:function(idx,total){
+            var left=parseInt(hd.children().eq(idx).offset().left);
+            var wid=parseInt(hd.children().eq(idx).width())
+            var view=$(window).width();
+            var ol=parseInt(hd.css("margin-left"));
+            if(left+wid>view){
+                var move=ol-left+view-wid;
+                hd.css({marginLeft:move});
+            }else if(left<0){
+                var cl=ol-left;
+                hd.css({marginLeft:cl});
+            }
+        }
+    });
 }
-TouchSlide({ slideCell:"#leftTabBox",endFun:function(){
-    console.log(0)
-}});
