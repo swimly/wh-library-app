75d9654d523a6caf6acd713ad601b3db3934461b
swimly <979741120@qq.com>
2016-06-02 19:05:40 +0800

    添加模块
    添加滑动侧边栏

--- js/main.js
+++ js/main.js
@@ -1,38 +1,79 @@
 /**
  * Created by 97974 on 2016/6/1.
  */
-init();
-TouchSlide({
-    slideCell:"#focus",
-    titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
-    mainCell:".bd ul",
-    effect:"left",
-    autoPlay:true,//自动播放
-    autoPage:true, //自动分页
-    switchLoad:"_src" //切换加载，真实图片路径为"_src"
-});
-function init(){
-    var hd=$(".tabBox .hd").find("ul");
-    var wid=0;
-    var wArr=[];
-    for(var i=0;i<hd.children().length;i++){
-        wid+=hd.children().eq(i).width();
-    }
-    hd.width(wid);
+$(function(){
+    init();
     TouchSlide({
-        slideCell:"#leftTabBox",
-        startFun:function(idx,total){
-            var left=parseInt(hd.children().eq(idx).offset().left);
-            var wid=parseInt(hd.children().eq(idx).width())
-            var view=$(window).width();
-            var ol=parseInt(hd.css("margin-left"));
-            if(left+wid>view){
-                var move=ol-left+view-wid;
-                hd.css({marginLeft:move});
-            }else if(left<0){
-                var cl=ol-left;
-                hd.css({marginLeft:cl});
+        slideCell:"#focus",
+        titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+        mainCell:".bd ul",
+        effect:"left",
+        autoPlay:true,//自动播放
+        autoPage:true, //自动分页
+        switchLoad:"_src" //切换加载，真实图片路径为"_src"
+    });
+    function init(){
+        var hd=$(".tabBox .hd").find("ul");
+        var wid=0;
+        var wArr=[];
+        for(var i=0;i<hd.children().length;i++){
+            wid+=hd.children().eq(i).width();
+        }
+        hd.width(wid);
+        TouchSlide({
+            slideCell:"#leftTabBox",
+            startFun:function(idx,total){
+                var left=parseInt(hd.children().eq(idx).offset().left);
+                var wid=parseInt(hd.children().eq(idx).width())
+                var view=$(window).width();
+                var ol=parseInt(hd.css("margin-left"));
+                if(left+wid>view){
+                    var move=ol-left+view-wid;
+                    hd.css({marginLeft:move});
+                }else if(left<0){
+                    var cl=ol-left;
+                    hd.css({marginLeft:cl});
+                }
             }
+        });
+    }
+    touch.on('.page-content', 'touchstart', function(ev){
+        ev.preventDefault();
+    });
+
+    var target = document.getElementById("page");
+    var btn=$("#page").find(".menu")
+    var dx, dy,sx,sy;
+    var width=$(window).width();
+    touch.on('#page', 'drag', function(ev){
+        dx = dx || 0;
+        dy = dy || 0;
+        sx = ev.x;
+        sy = ev.y;
+        var offx = dx + ev.x + "px";
+        var offy = dy + ev.y + "px";
+        if(parseInt(offx)<width*0.7 && parseInt(offx)>=0){
+            target.style.webkitTransform = "translate3d(" + offx + ",0,0)";
         }
     });
-}
+
+    touch.on('#page', 'dragend', function(ev){
+        dx = ev.distanceX;
+        if(parseInt(dx)<width*0.35){
+            target.style.webkitTransform = "translate3d(0,0,0)";
+            btn.removeClass("on")
+        }else{
+            target.style.webkitTransform = "translate3d("+(width*0.7)+"px"+",0,0)";
+            btn.addClass("on")
+        }
+    });
+    $(".menu").bind("click",function(){
+        if($(this).hasClass("on")){
+            target.style.webkitTransform = "translate3d(0,0,0)";
+            $(this).removeClass("on");
+        }else{
+            target.style.webkitTransform = "translate3d("+(width*0.7)+"px"+",0,0)";
+            $(this).addClass("on");
+        }
+    })
+})
