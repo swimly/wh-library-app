1a80fd6ecd4bf7754975a8164f8453b53f2bb665
swimly <979741120@qq.com>
2016-06-02 17:29:45 +0800

    app首页
    完成首页

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@
  * Created by 97974 on 2016/6/1.
  */
 init();
+TouchSlide({
+    slideCell:"#focus",
+    titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+    mainCell:".bd ul",
+    effect:"left",
+    autoPlay:true,//自动播放
+    autoPage:true, //自动分页
+    switchLoad:"_src" //切换加载，真实图片路径为"_src"
+});
 function init(){
     var hd=$(".tabBox .hd").find("ul");
     var wid=0;
