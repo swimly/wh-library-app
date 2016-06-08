9fdd8283ed9293a1585f3bba0774e0651fb1097b
swimly <979741120@qq.com>
2016-06-08 14:05:06 +0800

    触摸整体放大

--- js/seat.js
+++ js/seat.js
@@ -48,7 +48,7 @@ $(function(){
 		currentScale = initialScale + currentScale;
 		currentScale = currentScale > 2 ? 2 : currentScale;
 		currentScale = currentScale < 1 ? 1 : currentScale;
-		this.style.webkitTransform = 'scale(' + currentScale + ')';
+		target.style.webkitTransform = 'scale(' + currentScale + ')';
 		log("当前缩放比例为:" + currentScale + ".");
 	});
 
