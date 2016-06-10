50a4ad5e46690afadd907ca96dc15a54b3736c1e
swimly <979741120@qq.com>
2016-06-10 11:00:06 +0800

    在线选座

--- js/seat.js
+++ js/seat.js
@@ -10,22 +10,24 @@ $(function(){
 	var view=$('<div class="seat-view" id="viewHandle"></div>').appendTo("#thumb");
 	view.height($("#thumb").children().eq(0).height()*thumbBl)
 	.width(view.height()*viewBl)
-	console.log(thumbBl)
-	
 	touch.on('#seat', 'touchstart', function(ev){
-		ev.preventDefault();
+		/*ev.preventDefault();*/
 	});
-
 	var target = document.getElementById("seat");
 	var dx, dy;
-
+	var initialScale = 1;
+	var currentScale=1;
+	var total=$('#seat').find("li").length;
+	var selected=$('#seat').find(".selected").length;
+	$('#total').html(total)
+	$('#selected').html(total-selected)
 	touch.on('#seat', 'drag', function(ev){
 		dx = dx || 0;
 		dy = dy || 0;
 		/*console.log("当前x值为:" + dx + ", 当前y值为:" + dy +".");*/
 		var offx = dx + ev.x + "px";
 		var offy = dy + ev.y + "px";
-		target.style.webkitTransform = "translate3d(" + offx + "," + offy + ",0)";
+		target.style.webkitTransform = "translate3d(" + offx + "," + offy + ",0) scale("+currentScale+")";
 		var moveXbli=parseInt(offx)/wid;
 		var moveYbli=parseInt(offy)/colHeight;
 		var thumbWid=$("#thumb").width();
@@ -34,27 +36,29 @@ $(function(){
 		view.css({left:-moveXbli*thumbWid,top:-moveYbli*thumbHt})
 		console.log(moveXbli)
 	});
-
 	touch.on('#seat', 'dragend', function(ev){
 		dx += ev.x;
 		dy += ev.y;
 		console.log(dx,dy)
 	});
-	
-	var initialScale = 1;
-	var currentScale;
-
 	touch.on('#seat', 'pinchend', function(ev){
 		currentScale = ev.scale - 1;
 		currentScale = initialScale + currentScale;
+		document.getElementById("title").innerHTML=currentScale;
 		currentScale = currentScale > 2 ? 2 : currentScale;
-		currentScale = currentScale < 0.6 ? 0.6 : currentScale;
+		currentScale = currentScale < 0.8 ? 0.8 : currentScale;
 		target.style.webkitTransform = 'scale(' + currentScale + ')';
-		console.log("当前缩放比例为:" + currentScale + ".");
-		target.style.webkitTransform = "translate3d(" + dx*currentScale + "," + dy*currentScale + ",0)";
-	});
-
-	touch.on('#seat', 'pinchend', function(ev){
+		var view=document.getElementById("viewHandle");
+		view.style.webkitTransform="scale("+(1/currentScale)+")"
 		initialScale = currentScale;
 	});
+	$('#seat').find('li').click(function(){
+		if($(this).hasClass('selected')){
+			alert('已选')
+		}else if($(this).hasClass('active')){
+			$(this).removeClass('active')
+		}else{
+			$(this).addClass('active')
+		}
+	})
 })
